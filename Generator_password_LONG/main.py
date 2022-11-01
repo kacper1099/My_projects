e749ba8c33186c79f927c5ceadffc360e1c10116
Kacper <kacper.galon@onet.pl>
2022-11-01 10:14:17 +0100

    Hangmam game

---
+++ Generator_password_LONG/main.py
@@ -0,0 +1,77 @@
+import sys
+import random
+import string
+import re
+password=[]
+characters_left=-1
+
+def update_characters_left(number_of_charakters):
+    global characters_left
+    if number_of_charakters < 0 or number_of_charakters > characters_left:
+        print("Liczba znaków jest spoza przedziału 0, ", characters_left)
+        sys.exit(0)
+    else:
+        characters_left -= number_of_charakters
+        print("Pozostało znaków: ",characters_left )
+
+while True:
+    while True:
+        password_length=input("Jak długie ma być hasło?: ")
+        if not re.match(r"^[A-Za-z/]", password_length):
+            break
+    int_password_length=int(password_length)
+    if int_password_length < 5:
+        print("Hasło musi mieć minimum 5 znaków,spróbuj jeszcze raz")
+    elif int_password_length>=5:
+        characters_left = int_password_length
+        break
+while True:
+    lowercase_letters=int(input("Ile małych liter ma mieć hasła?: "))
+    if re.match(r"^[0-9]", str(lowercase_letters)):
+        break
+update_characters_left(lowercase_letters)
+
+while True:
+    uppercase_letters=int(input("Ile dużych liter ma mieć hasła?: "))
+    if re.match(r"^[0-9]", str(uppercase_letters)):
+        break
+    else:
+        print("Podaj lizcbę:")
+update_characters_left(uppercase_letters)
+
+while True:
+    special_charakters=int(input("Ile znaków specjalnych ma mieć hasła?: "))
+    if re.match(r"^[0-9]", str(special_charakters)):
+        break
+
+while True:
+    digits=int(input("Ile cyfr ma mieć hasło?:"))
+    if re.match(r"^[0-9]", str(digits)):
+        break
+
+if characters_left>0:
+    print("Nie wszystkie znaki zostały wkorzystane, hasło zostanie uzupełnione małymi literami")
+    lowercase_letters+=characters_left
+
+print()
+print("Długość hasła: ", password)
+print("Duże litery: ", uppercase_letters)
+print("Małe litery: ", lowercase_letters)
+print("Znaki specialne: ", special_charakters)
+print("Cyfry: ", digits)
+
+for i in range(int_password_length):
+    if lowercase_letters>0:
+        password.append(random.choice(string.ascii_lowercase))
+        lowercase_letters-=1
+    if uppercase_letters>0:
+        password.append((random.choice(string.ascii_uppercase)))
+        uppercase_letters-=1
+    if special_charakters>0:
+        password.append(random.choice(string.punctuation))
+        special_charakters-=1
+    if digits>0:
+        password.append(random.choice(string.digits))
+        digits-=1
+random.shuffle(password)
+print("Wygenerowane hasło: ", "".join(password))
---
+++ Hangman/main.py
@@ -0,0 +1,48 @@
+
+import sys
+import re
+no_of_tries=5
+while True:
+    word = input("Podaj słowo do gry: ")
+    if re.match("^[A-Za-z]*$", word):
+        break
+
+used_letters=[]
+user_word=[]
+
+def find_indexes(word,letter):
+    indexes=[]
+
+    for index, letter_in_word in enumerate(word):
+        if letter == letter_in_word:
+            indexes.append(index)
+    return indexes
+def show_state_of_game():
+    print()
+    print(user_word)
+    print("Pozostało prób: ", no_of_tries)
+    print("Użyte litery: ", used_letters)
+    print()
+for _ in word:
+    user_word.append("_")
+while True:
+    letter=input("Podaj literę: ")
+    used_letters.append(letter)
+
+    found_indexes=find_indexes(word,letter)
+    if len(found_indexes)==0:
+        print("Nie ma takiej litery")
+        no_of_tries-=1
+
+
+        if no_of_tries==0:
+            print("Koniec gry :(")
+            sys.exit(0)
+    else:
+        for index in found_indexes:
+            user_word[index]=letter
+
+        if "".join(user_word)==word:
+            print("Wygrana!!!")
+            sys.exit(0)
+    show_state_of_game()
